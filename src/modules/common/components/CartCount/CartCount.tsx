2c41e4dbd957f21247f81ec57de99640b0c37a30
kzinthant <kzinthant@gmail.com>
2022-02-01 10:08:50 +0630

    cart count finished

--- src/modules/common/components/CartCount/CartCount.tsx
+++ src/modules/common/components/CartCount/CartCount.tsx
@@ -5,6 +5,9 @@ import styles from './CartCount.module.css';
 function CartCount(): React.ReactElement {
   return (
     <div className={styles.cart}>
+      <div className={styles.cart__number_back}>
+        3
+      </div>
       <Cart />
     </div>
   );
