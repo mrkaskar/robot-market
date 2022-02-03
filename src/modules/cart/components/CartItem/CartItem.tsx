6d5fcf843ccc38e69826833d7c3b483c73fcbeb2
kzinthant <kzinthant@gmail.com>
2022-02-03 17:42:58 +0630

    cart ui

--- src/modules/cart/components/CartItem/CartItem.tsx
+++ src/modules/cart/components/CartItem/CartItem.tsx
@@ -1,16 +1,48 @@
 import React from 'react';
 import styles from 'modules/cart/components/CartItem/CartItem.module.css';
-import { ICart } from 'modules/cart/state/cartState';
+import NumberCounter from 'modules/common/components/NumberCounter/NumberCounter';
+
+interface ICartItem {
+  name: string
+  count: number
+  price: string
+  img: string
+}
 
 function CartItem({
-  name,
-  img,
-  count,
-  price,
-}: ICart): React.ReactElement {
+  name, count, price, img,
+}: ICartItem): React.ReactElement {
   return (
-    <div className={styles.cart_item}>
-      {name}
+    <div className={styles.cart__segments}>
+      <div className={styles.cart__segments_one}>
+        <div className={styles.cart__item_left}>
+          <img src={img} alt="robot" className={styles.cart__item_img} />
+          <div className={styles.cart__item_info}>
+            <div className={styles.cart__item_name}>{name}</div>
+            <div className={styles.cart__item_price}>
+              ฿
+              {' '}
+              {price}
+              {' '}
+              per unit
+            </div>
+            <div>
+              x
+              <span style={{ marginRight: '10px' }} />
+              <NumberCounter
+                number={count}
+                onIncrease={() => undefined}
+                onDecrease={() => undefined}
+              />
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div className={styles.subtotal}>
+        ฿ 100
+      </div>
     </div>
   );
 }
--- src/modules/cart/components/CartWrapper/CartWrapper.tsx
+++ src/modules/cart/components/CartWrapper/CartWrapper.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import styles from 'modules/cart/components/CartWrapper/CartWrapper.module.css';
-import globalStyles from 'global/Global.module.css';
 import useCart from 'modules/cart/hooks/useCart';
 import CartCount from 'modules/common/components/CartCount/CartCount';
 import useMobileSize from 'hooks/useMobileSize';
+import colors from 'global/colors/colors';
+import CartItem from '../CartItem/CartItem';
 
 function Cart(): React.ReactElement {
   const { carts } = useCart();
   const { isMobile } = useMobileSize();
   return (
-    <div className={styles.cart__area}>
+    <div
+      className={styles.cart__area}
+      style={{
+        width: `${isMobile ? '260px' : '290px'}`,
+      }}
+    >
+
       <div
         className={styles.cart__header}
       >
@@ -27,14 +34,24 @@ function Cart(): React.ReactElement {
             </span>
           )
           : (
-            <div>
+            <div className={styles.cart__items}>
+              <div
+                className={styles.cart__segments}
+                style={{
+                  color: colors.inactive,
+                }}
+              >
+                <div className={styles.cart__segments_one}>Item Description</div>
+                <div className={styles.cart__segments_two}>Subtotal</div>
+              </div>
               {
                 Object.keys(carts).map((item) => (
-                  <div>
-                    <p>{carts[item].name}</p>
-                    <img src={carts[item].img} alt="robot" />
-                    <p>{carts[item].count}</p>
-                  </div>
+                  <CartItem
+                    name={item}
+                    img={carts[item].img}
+                    price={carts[item].price}
+                    count={carts[item].count}
+                  />
                 ))
               }
             </div>
