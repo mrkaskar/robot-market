a76f0b9fb775d11230bb97ab9a03f9a5e82cd8fb
kzinthant <kzinthant@gmail.com>
2022-02-04 00:43:15 +0630

    favicon added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ import styles from './Header.module.css';
 function Header(): React.ReactElement {
   return (
     <div className={styles.header}>
-      <Logo />
+      <Logo className={styles.logo} />
       <h1 className={styles.header__title} style={{ fontFamily: 'poppins', color: colors.primary }}>Robot Market</h1>
     </div>
   );
